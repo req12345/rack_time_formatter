34ee10663cf1915f63b158c2e2671e5b58c9b816
req12345 <ibahmetov@inno.co>
2024-02-18 17:39:03 +0300

    refactoring readme path 4

--- middleware/runtime.rb
+++ middleware/runtime.rb
@@ -1,15 +1,15 @@
 # frozen_string_literal: true
 
 class Runtime
-    def initialize(app)
-        @app = app
-    end
+  def initialize(app)
+      @app = app
+  end
 
-    def call(env)
-        start = Time.now
-        status, headers, body = @app.call(env)
-        headers['X-Runtime'] = "%fs" % (Time.now - start)
+  def call(env)
+    start = Time.now
+    status, headers, body = @app.call(env)
+    headers['X-Runtime'] = "%fs" % (Time.now - start)
         
-        [status, headers, body]
-    end
+    [status, headers, body]
+  end
 end
